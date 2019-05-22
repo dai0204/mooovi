4ebdd6cde3068b9e0dd9417637ef3e06eb38a314
dai0204 <nandai0204@gmail.com>
2019-05-22 17:37:10 +0900

    インクリメンタルサーチ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_102350) do
+ActiveRecord::Schema.define(version: 2019_05_22_074528) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_01_22_102350) do
     t.string "director"
     t.text "detail"
     t.string "open_date"
+    t.index ["title"], name: "index_products_on_title"
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
